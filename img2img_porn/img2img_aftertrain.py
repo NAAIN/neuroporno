f8e04b6ac75614688a180cdad199dbfc16b9120b
NAAIN <ueongos88z@gmail.com>
2024-07-27 14:48:11 +0300

    перепахивание по второму кругу

--- img2img_porn/img2img_aftertrain.py
+++ img2img_porn/img2img_aftertrain.py
@@ -2,10 +2,10 @@ import torch
 import torch.nn as nn
 from torchvision import transforms
 from PIL import Image
-import os
 
-inputImage = "a.png"
-outputImage = "a_out.png"
+inputImage = "resources/a.png"
+outputImage = "resources/a_out.png"
+modelFile = "models/img2img_degenerat.pth"
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
@@ -15,13 +15,7 @@ class Autoencoder(nn.Module):
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
         )
-        # self.hidden = nn.Sequential(
-        #     nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        # )
         self.decoder = nn.Sequential(
-            # nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
-            # nn.ReLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
@@ -30,16 +24,15 @@ class Autoencoder(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        # x = self.hidden(x)
         x = self.decoder(x)
         return x
 
 model = Autoencoder()
-model = torch.load('img2img_degenerat.pth')
+model = torch.load(modelFile)
 #model.eval()
 
 transform = transforms.Compose([
-    #transforms.Resize((1024, 1024)),
+    transforms.Resize((1024, 1024)),
     transforms.ToTensor(),
 ])
 
@@ -48,12 +41,7 @@ image = Image.open(inputImage).convert("RGB")
 image = transform(image)
 image = image.unsqueeze(0)
 
-with torch.no_grad():
-    output = model(image)
-    output = model(output)
-    output = model(output)
-    output = model(output)
-    output = model(output)
+with torch.no_grad(): output = model(image)
 
 # Преобразование и сохранение выходного изображения
 output = output.squeeze(0)
--- img2img_porn/img2img_train.py
+++ img2img_porn/img2img_train.py
@@ -5,7 +5,18 @@ from torch.utils.data import DataLoader, Dataset
 from torchvision import transforms
 from PIL import Image
 import os
-
+# КРУТИЛАЧКИИИ
+batch_size = 1000
+learn_rate = 0.0001
+num_epochs = 1200
+datasetDir = "resources/img2img_dataset/"
+ExistsModelPthName = ""
+#ExistsModelPthName = "models/img2img_degenerat.pth" #закомментируй чтобы создать новую модель
+LearnedPthName = "models/img2img_degenerat.pth"
+debug_in_name = True
+Shuffle = True
+#^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+# Код ниже лучше не трогать (говнокод warning)
 class CustomDataset(Dataset):
     def __init__(self, img_dir, transform=None):
         self.img_dir = img_dir
@@ -27,8 +38,8 @@ transform = transforms.Compose([
     transforms.Resize((512, 512)),
     transforms.ToTensor(),
 ])
-dataset = CustomDataset(img_dir='/mnt/ata-ST500LM030/neironke/ComfyUI-master/output', transform=transform)
-dataloader = DataLoader(dataset, batch_size=10, shuffle=True)
+dataset = CustomDataset(datasetDir,transform)
+dataloader = DataLoader(dataset, batch_size,Shuffle)
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
@@ -38,13 +49,7 @@ class Autoencoder(nn.Module):
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
         )
-        # self.hidden = nn.Sequential(
-        #     nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),
-        #     nn.ReLU(),
-        # )
         self.decoder = nn.Sequential(
-            # nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1),
-            # nn.ReLU(),
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1),
             nn.ReLU(),
             nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1),
@@ -53,16 +58,13 @@ class Autoencoder(nn.Module):
 
     def forward(self, x):
         x = self.encoder(x)
-        # x = self.hidden(x)
         x = self.decoder(x)
         return x
 
-
 model = Autoencoder()
-if os.path.isfile('img2img_degenerat.pth'): model = torch.load('img2img_degenerat.pth')
+if os.path.isfile(ExistsModelPthName): model = torch.load(ExistsModelPthName)
 criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
-num_epochs = 30
+optimizer = optim.Adam(model.parameters(),learn_rate)
 
 try:
     for epoch in range(num_epochs):
@@ -76,4 +78,5 @@ try:
         print(f'Epoch [{epoch}/{num_epochs}], Loss: {loss.item():.4f}')
 except KeyboardInterrupt:
     print("та за шо((")
-torch.save(model,"img2img_degenerat.pth")
+if debug_in_name: LearnedPthName += f"{epoch}_{learn_rate}"
+torch.save(model,"models/img2img_degenerat.pth")
