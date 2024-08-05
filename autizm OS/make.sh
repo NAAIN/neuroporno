f1386d68c46381bf203a544f9b0a7cbb83b02ad9
NAAIN <ueongos88z@gmail.com>
2024-08-05 23:47:10 +0300

    потыкал скрипты

--- autizm OS/make.sh
+++ autizm OS/make.sh
@@ -1,5 +1,5 @@
 nasm -f elf32 kernel.asm -o kasm.o
-gcc -fno-stack-protector -m32 -c kernel.c -o bin/kc.o
-ld -m elf_i386 -T link.ld -o kernel kasm.o bin/kc.o
+gcc -fno-stack-protector -m32 -c kernel.c -o ../binaries/kc.o
+ld -m elf_i386 -T link.ld -o kernel kasm.o ../binaries/kc.o
 qemu-system-i386 -kernel kernel
 
